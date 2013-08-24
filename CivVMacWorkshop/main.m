e7718b8746f50fa2e5578041b11e153c36a3b5f6
Brad Allred <bradallred@me.com>
2013-08-24 10:40:36 -0600

    Add Xcode project

---
+++ CivVMacWorkshop/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CivVMacWorkshop
+//
+//  Created by Brad on 13/8/24.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
